4745ade2f08ec729c4bc487e6f6bcd32187c56d2
terrorist-squad <100994062+terrorist-squad@users.noreply.github.com>
2022-03-12 10:48:47 +0100

    Update settings.py

--- interface/config/settings.py
+++ interface/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k4_4oej-+#*ihbkthpjgwj8+0ygi+4p1^6aj3jsox(-f7u35=z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
