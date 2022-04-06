674b23e4e0e86d4e208458930540b737c463d835
CharlieScene <100994062+terrorist-squad@users.noreply.github.com>
2022-04-06 21:18:31 +0200

    Update docker_adapter.py
    change uid and gid to USER_UID and USER_GID

--- interface/utils/adapter/adapter/docker_adapter.py
+++ interface/utils/adapter/adapter/docker_adapter.py
@@ -5,10 +5,12 @@ class DockerAdapter(object):
    def stopInstance(self, containerName):
         return "docker exec '{}' pkill -int ffmpeg &".format(containerName)
 
-   def startInstance(self, media_path, containerName, title, limit_in_gb, imageName):
+   def startInstance(self, media_path, containerName, title, limit_in_gb, imageName, UID, GID):
         limit_in_kb = int( int( limit_in_gb ) * 1024 * 1024)
-        return "docker run -d -e LIMIT_MAXIMUM_FOLDER_KB={} --rm -v {}:/output --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ &".format(
+        return "docker run -d -e LIMIT_MAXIMUM_FOLDER_KB={} -e UID={} -e GID={} --rm -v {}:/output --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ &".format(
             limit_in_kb,
+            UID,
+            GID,
             media_path, 
             containerName, 
             imageName,
