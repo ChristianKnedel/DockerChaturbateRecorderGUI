b7bf775c1baf827eaf55fa4bb605e1df5ed2aa91
CharlieScene <100994062+terrorist-squad@users.noreply.github.com>
2022-04-09 19:39:09 +0200

    Update docker_adapter.py
    #8 fix: the wrong parameter was passed for the size limit

--- interface/utils/adapter/adapter/docker_adapter.py
+++ interface/utils/adapter/adapter/docker_adapter.py
@@ -7,7 +7,7 @@ class DockerAdapter(object):
 
    def startInstance(self, media_path, containerName, title, limit_in_gb, imageName, UID, GID):
         limit_in_kb = int( int( limit_in_gb ) * 1024 * 1024)
-        return "docker run -d -e LIMIT_MAXIMUM_FOLDER_KB={} -e UID={} -e GID={} --rm -v {}:/output --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ &".format(
+        return "docker run -d -e MAXIMUM_FOLDER_KB={} -e UID={} -e GID={} --rm -v {}:/output --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ &".format(
             limit_in_kb,
             UID,
             GID,
