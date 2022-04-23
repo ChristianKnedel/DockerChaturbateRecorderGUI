cc260533b5cb2682bb25644d6fd2b40a05eba1c3
Christian Knedel <c.knedel@365layouts.de>
2022-04-23 21:26:46 +0200

    new features# #11 and fix for #14

--- interface/utils/adapter/adapter/docker_adapter.py
+++ interface/utils/adapter/adapter/docker_adapter.py
@@ -6,12 +6,13 @@ class DockerAdapter(object):
         return "docker exec '{}' pkill -int ffmpeg &".format(containerName)
 
    def startInstance(self, media_path, containerName, title, limit_in_gb, imageName, UID, GID):
-        return "docker run -d -e MAXIMUM_FOLDER_GB={} -e UID={} -e GID={} --rm -v {}:/code/videos/ --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ &".format(
+        return "docker run -d -e MAXIMUM_FOLDER_GB={} -e UID={} -e GID={} --rm -v {}:/code/videos/ --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ -c {} &".format(
             limit_in_gb,
             UID,
             GID,
             media_path, 
             containerName, 
             imageName,
-            title
+            title,
+            containerName
         )
