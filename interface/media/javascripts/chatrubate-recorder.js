20e216166b94ec0f79362f3e6025b87d03964bf6
Christian Knedel <c.knedel@365layouts.de>
2022-04-28 20:39:00 +0200

    #11 save color mode

--- interface/media/javascripts/chatrubate-recorder.js
+++ interface/media/javascripts/chatrubate-recorder.js
@@ -69,10 +69,34 @@ const recorder = function(toolbar = false, wishlist = false, videolist = false) 
     jQuery(this).on( "wishlist:change", this.refreshWishlist.bind(this));
     this.refreshWishlist();
     
+    $("body").attr('class', this.loadSetting('color-mode'))
+    
     this.heartbeat = setInterval(this.refrashAll.bind(this), 5000);
+}
+recorder.prototype.loadSetting = function(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+recorder.prototype.saveSetting = function(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 recorder.prototype.toggleDarkMode = function() {
     $("body").toggleClass("dark", "");
+    this.saveSetting('color-mode', $("body").attr('class'), 100000);
 }
 recorder.prototype.refrashAll = function() {
     jQuery(this).trigger( "wishlist:change" );
