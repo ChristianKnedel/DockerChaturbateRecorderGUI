0280937ab6ab7ab85b49b03dd8f604739a0d1f91
Dethkiller15 <49108931+Dethkiller15@users.noreply.github.com>
2024-09-01 09:41:13 -0500

    Update shell_adapter.py
    added same checks as done for docker_adapter.py

--- interface/utils/adapter/adapter/shell_adapter.py
+++ interface/utils/adapter/adapter/shell_adapter.py
@@ -8,8 +8,34 @@ class ShellAdapter(object):
 
 
    def startInstance(self, media_path, containerName, title, limit_in_gb, imageName, UID, GID, resolution):
-        return "/recorder/recorder.sh -u https://chaturbate.com/{}/ -c {} -r {} &".format(
-            title,
-            containerName,
-            resolution
-        )
+        # Form the URL using the title
+        url = f"https://chaturbate.com/{title}/"
+
+        # Fetch the HTML content from the URL
+        response = requests.get(url)
+        html_content = response.text
+
+        # Find all occurrences of URLs ending with .m3u8
+        streams = re.findall(r'https://[a-zA-Z0-9.+-_:/]*\.m3u8', html_content)
+
+        if urls:
+            stream = re.sub(r'\\u([0-9A-Fa-f]{4})', lambda m: chr(int(m.group(1), 16)), streams[0])
+            # Return the Docker command if .m3u8 URLs are found
+            FoundStreamVar = "Found stream for {}.".format(
+                title
+            )
+            # print(FoundStreamVar)
+            return "/recorder/recorder.sh -u {} -c {} -r {} &".format(
+                stream,
+                containerName,
+                resolution
+            )
+        else:
+            StreamNotFoundVar = "No stream found for {}.".format(
+                title
+            )
+            # print(StreamNotFoundVar)
+            
+            return "echo No Stream found for title {}. Skipping. > /dev/null 2>&1".format(
+                title
+            )
