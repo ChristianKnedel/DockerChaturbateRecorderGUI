17c93f06c9a701336a0d31446f3c5634c6b58426
Dethkiller15 <49108931+Dethkiller15@users.noreply.github.com>
2024-09-01 09:36:59 -0500

    Update docker_adapter.py
    Added check to make sure the streamer is streaming and if so pass the stream URL obtained via that process to the recorder and if not send blank null command so that it doesn't clog logs
    
    you can uncomment the print commands if you want to see if a specific streamer is streaming in console but odds are you will just be checking the GUI anyways.

--- interface/utils/adapter/adapter/docker_adapter.py
+++ interface/utils/adapter/adapter/docker_adapter.py
@@ -1,3 +1,6 @@
+import requests
+import re
+
 class DockerAdapter(object):
    def getInstances(self, prefix):
         return "docker container ls --format '{{.Names}}' | grep '" + prefix + "'"
@@ -6,14 +9,40 @@ class DockerAdapter(object):
         return "docker exec '{}' pkill -int ffmpeg &".format(containerName)
 
    def startInstance(self, media_path, containerName, title, limit_in_gb, imageName, UID, GID, resolution):
-        return "docker run -d -e LIMIT_MAXIMUM_FOLDER_GB={} -e UID={} -e GID={} --rm -v {}:/code/videos/ --name {} {} /code/recorder.sh -u https://chaturbate.com/{}/ -c {} -r {} &".format(
-            limit_in_gb,
-            UID,
-            GID,
-            media_path, 
-            containerName, 
-            imageName,
-            title,
-            containerName,
-            resolution
-        )
+        # Form the URL using the title
+        url = f"https://chaturbate.com/{title}/"
+
+        # Fetch the HTML content from the URL
+        response = requests.get(url)
+        html_content = response.text
+
+        # Find all occurrences of URLs ending with .m3u8
+        streams = re.findall(r'https://[a-zA-Z0-9.+-_:/]*\.m3u8', html_content)
+
+        if streams:
+            stream = re.sub(r'\\u([0-9A-Fa-f]{4})', lambda m: chr(int(m.group(1), 16)), streams[0])
+            # Return the Docker command if .m3u8 URLs are found
+            FoundStreamVar = "Found stream for {}.".format(
+                title
+            )
+            #print(FoundStreamVar)
+            return "docker run -d -e LIMIT_MAXIMUM_FOLDER_GB={} -e UID={} -e GID={} --rm -v {}:/code/videos/ --name {} {} /code/recorder.sh -u {} -c {} -r {} &".format(
+                limit_in_gb,
+                UID,
+                GID,
+                media_path, 
+                containerName, 
+                imageName,
+                stream,
+                containerName,
+                resolution
+            )
+        else:
+            StreamNotFoundVar = "No stream found for {}.".format(
+                title
+            )
+            # print(StreamNotFoundVar)
+            
+            return "echo No Stream found for title {}. Skipping. > /dev/null 2>&1".format(
+                title
+            )
